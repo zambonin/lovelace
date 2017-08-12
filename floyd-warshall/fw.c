48f4eb5aa0be3d7fc826998eb0392d674fd83d7b
Gustavo Zambonin <gzmbnn@gmail.com>
2017-08-12 17:35:28 -0300

    fw: number of vertices can now be greater than uint8_t * various valgrind and clang-tidy/format fixes * readme updated

--- floyd-warshall/fw.c
+++ floyd-warshall/fw.c
@@ -1,42 +1,37 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
 
-#define INFINITY_LIKE 99999
-
 void _time(double *t) {
-
   static int sec = -1;
   struct timeval tv;
 
-  gettimeofday(&tv, (struct timezone *) 0);
+  gettimeofday(&tv, (struct timezone *)0);
 
   if (sec < 0) {
     sec = tv.tv_sec;
   }
   *t = (tv.tv_sec - sec) + 1.0e-6 * tv.tv_usec;
-
 }
 
-// Solves the all-pairs shortest path problem using Floyd Warshall algorithm
-void floydWarshall (uint32_t* dists, uint32_t v) {
-
+void floydWarshall(uint32_t *dists, uint32_t v) {
   uint32_t sum = 0;
   double start = 0, start_loop_1 = 0, start_loop_2 = 0, start_loop_3 = 0,
          end = 0, end_loop_1 = 0, end_loop_2 = 0, end_loop_3 = 0;
   _time(&start);
 
-  for (uint8_t i = 0; i < v; ++i) {
+  for (uint32_t i = 0; i < v; ++i) {
     _time(&start_loop_1);
-    for (uint8_t f = 0; f < v; ++f) {
+    for (uint32_t f = 0; f < v; ++f) {
       _time(&start_loop_2);
-      for (uint8_t d = 0; d < v; ++d) {
+      for (uint32_t d = 0; d < v; ++d) {
         _time(&start_loop_3);
-        if (!__builtin_uadd_overflow(dists[f*v + i], dists[i*v + d], &sum)
-            && sum < dists[f*v + d]) {
-          dists[f*v + d] = sum;
+        if (!__builtin_uadd_overflow(dists[f * v + i], dists[i * v + d],
+                                     &sum) &&
+            sum < dists[f * v + d]) {
+          dists[f * v + d] = sum;
         }
         _time(&end_loop_3);
       }
@@ -46,76 +41,68 @@ void floydWarshall (uint32_t* dists, uint32_t v) {
   }
   _time(&end);
 
-  printf("FW: %.8f\nInner Comp. Second Loop: %.8f\n\
-Inner Comp. Third Loop: %.8f\nInner Comp.: %.20f\n",
-      end - start, end_loop_1 - start_loop_1,
-      end_loop_2 - start_loop_2, end_loop_3 - start_loop_3);
-
+  printf("FW: %.8f\nInner Comp. Second Loop: %.8f\n"
+         "Inner Comp. Third Loop: %.8f\nInner Comp.: %.20f\n",
+         end - start, end_loop_1 - start_loop_1, end_loop_2 - start_loop_2,
+         end_loop_3 - start_loop_3);
 }
 
-/* A utility function to print solution */
-void printSolution (uint32_t* dists, uint32_t v) {
-    uint32_t i, j;
-
-    printf ("All pair shortest distances\n");
-    for (i = 0; i < v; ++i) {
-        for (j = 0; j < v; ++j) {
-            if (dists[i*v+j] > INFINITY_LIKE) printf("%7s", "inf");
-            else printf ("%7u", dists[i*v+j]);
-        }
-        printf("\n");
+void printSolution(uint32_t *dists, uint32_t v) {
+  printf("All pair shortest distances\n");
+  for (uint32_t i = 0; i < v; ++i) {
+    for (uint32_t j = 0; j < v; ++j) {
+      if (dists[i * v + j] == UINT32_MAX) {
+        printf("%7s", "inf");
+      } else {
+        printf("%7u", dists[i * v + j]);
+      }
     }
+    printf("\n");
+  }
 }
 
-// Main program - reads input, calls FW, shows output
-int main (int argc, char* argv[]) {
+int32_t main(int32_t argc, char **argv) {
+  double start_prg, end_prg;
+  _time(&start_prg);
 
-    double start_prg, end_prg;
-    _time(&start_prg);
+  if (argc < 2) {
+    printf("Missing input filename.\n");
+    exit(EXIT_FAILURE);
+  }
 
-    //Checks if filename has been passed
-    if (argc < 2) {
-        printf("Missing input filename.\n");
-        exit(1);
+  FILE *input = fopen(argv[1], "r");
+  if (input == NULL) {
+    printf("Missing file \"%s\"\n", argv[1]);
+    exit(EXIT_FAILURE);
+  }
+
+  uint32_t v, e;
+  fscanf(input, "%u %u", &v, &e);
+
+  uint32_t *dists = malloc(v * v * sizeof(uint32_t)), source, dest, cost, i;
+  if (dists == NULL) {
+    printf("Matrix is too big!\n");
+    exit(EXIT_FAILURE);
+  }
+  memset(dists, UINT32_MAX, v * v * sizeof(uint32_t));
+
+  for (i = 0; i < e; ++i) {
+    fscanf(input, "%u %u %u", &source, &dest, &cost);
+    if (cost < dists[source * v + dest]) {
+      dists[source * v + dest] = cost;
     }
+  }
+  fclose(input);
 
-    //Tries to open the input file
-    FILE* input = fopen(argv[1], "r");
-    if(!input){
-        printf("Missing file \"%s\"\n",argv[1]);
-        exit(1);
-    }
+  for (i = 0; i < v; ++i) {
+    dists[i * v + i] = 0;
+  }
 
-    //Reads input file
-    //First line: v (number of vertices) and e (number of edges)
-    uint32_t v, e;
-    fscanf(input, "%u %u", &v, &e);
+  floydWarshall(dists, v);
+  printSolution(dists, v);
+  _time(&end_prg);
+  printf("Program Execution Time: %.8f\n", end_prg - start_prg);
 
-    //Allocates distances matrix (w/ size v*v) i
-    //and sets it with max distance and 0 for own vertex
-    uint32_t* dists = malloc(v*v*sizeof(uint32_t));
-    memset(dists, UINT32_MAX, v*v*sizeof(uint32_t));
-    uint32_t i;
-    for ( i = 0; i < v; ++i ) dists[i*v+i] = 0;
-
-    //Reads edges from file and sets them in the distance matrix
-    uint32_t source, dest, cost;
-    for ( i = 0; i < e; ++i ){
-        fscanf(input, "%u %u %u", &source, &dest, &cost);
-        if (cost < dists[source*v+dest]) dists[source*v+dest] = cost;
-    }
-
-    fclose(input);
-
-    //Computes the solution
-    floydWarshall( dists, v );
-
-    //Prints the solution
-    /* printSolution (dists, v); */
-
-    _time(&end_prg);
-    printf("Program Execution Time: %.8f\n", end_prg - start_prg);
-
-    return 0;
-
+  free(dists);
+  exit(EXIT_SUCCESS);
 }
--- floyd-warshall/generator.c
+++ floyd-warshall/generator.c
@@ -1,51 +1,37 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
 
-// Main program - reads parameters, generates graph
-int main (int argc, char* argv[]) {
+int32_t main(int32_t argc, char **argv) {
+  if (argc < 6) {
+    printf("Missing parameters.\nUsage: %s [#vertices] [#edges] [#max cost] "
+           "[rand seed] [output file name]\n",
+           argv[0]);
+    exit(EXIT_FAILURE);
+  }
 
-    //Checks if parameters have been passed
-    if (argc < 6) {
-        printf("Missing parameters.\nUsage: %s [#vertices] [#edges] [#max cost] [rand seed] [output file name]\n",argv[0]);
-        exit(1);
-    }
+  uint32_t v = strtoul(argv[1], NULL, 0), e = strtoul(argv[2], NULL, 0),
+           max = strtoul(argv[3], NULL, 0), seed = strtoul(argv[4], NULL, 0);
+  FILE *output = fopen(argv[5], "w");
 
-    //inputs
-    uint32_t v, e, max_cost, seed;
-    v = atoi(argv[1]);
-    e = atoi(argv[2]);
-    max_cost = atoi(argv[3]);
-    if (max_cost == 0) {
-        printf("Max cost must be greater than zero.\n");
-        exit(1);
-    }
-    seed = atoi(argv[4]);
-    srand(seed);
+  if (v == UINT32_MAX || e == UINT32_MAX || max == UINT32_MAX ||
+      seed == UINT32_MAX || output == NULL) {
+    printf("Invalid parameter(s).\n");
+    exit(1);
+  }
 
-    //Tries to open the output file
-    FILE* output = fopen(argv[5], "w");
-    if(!output){
-        printf("Could not open file \"%s\"\n",argv[5]);
-        exit(1);
-    }
+  fprintf(output, "%u %u\n", v, e);
+  srandom(seed);
 
-    //Writes output file
-    //First line: v (number of vertices) and e (number of edges)
-    fprintf(output, "%u %u\n", v, e);
+  uint32_t source, dest, cost, i;
+  for (i = 0; i < e; ++i) {
+    source = random() % v;
+    dest = random() % v;
+    cost = random() % max;
+    fprintf(output, "%u %u %u\n", source, dest, cost);
+  }
 
-    //Generates edges to write to file
-    uint32_t source, dest, cost;
-    uint32_t i;
-    for ( i = 0; i < e; ++i ){
-        source = rand() % v;
-        dest = rand() % v;
-        cost = rand() % max_cost;
-        fprintf(output, "%u %u %u\n", source, dest, cost);
-    }
-
-    fclose(output);
-
-    return 0;
+  fclose(output);
+  exit(EXIT_SUCCESS);
 }
