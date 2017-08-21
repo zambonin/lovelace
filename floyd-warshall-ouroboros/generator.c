3fbfcd41b44c53f3bb0d5fe32b732dffe1ebff5a
Gustavo Zambonin <gzmbnn@gmail.com>
2017-08-21 15:10:29 -0300

    added python implementation * various valgrind and clang fixes

--- floyd-warshall-ouroboros/generator.c
+++ floyd-warshall-ouroboros/generator.c
@@ -1,51 +1,39 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
 
-// Main program - reads parameters, generates graph
-int main (int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
-    //Checks if parameters have been passed
-    if (argc < 6) {
-        printf("Missing parameters.\nUsage: %s [#vertices] [#edges] [#max cost] [rand seed] [output file name]\n",argv[0]);
-        exit(1);
-    }
+  if (argc < 6) {
+    printf("Missing parameters.\nUsage: %s [#vertices] [#edges] [#max cost] "
+           "[rand seed] [output file name]\n",
+           argv[0]);
+    exit(1);
+  }
 
-    //inputs
-    uint32_t v, e, max_cost, seed;
-    v = atoi(argv[1]);
-    e = atoi(argv[2]);
-    max_cost = atoi(argv[3]);
-    if (max_cost == 0) {
-        printf("Max cost must be greater than zero.\n");
-        exit(1);
-    }
-    seed = atoi(argv[4]);
-    srand(seed);
+  uint32_t v = strtol(argv[1], NULL, 0), e = strtol(argv[2], NULL, 0),
+           max = strtol(argv[3], NULL, 0), seed = strtol(argv[4], NULL, 0);
+  FILE *output = fopen(argv[5], "w");
 
-    //Tries to open the output file
-    FILE* output = fopen(argv[5], "w");
-    if(!output){
-        printf("Could not open file \"%s\"\n",argv[5]);
-        exit(1);
-    }
+  if (v == UINT32_MAX || e == UINT32_MAX || max == UINT32_MAX ||
+      seed == UINT32_MAX || output == NULL) {
+    printf("Invalid parameter(s).\n");
+    exit(1);
+  }
 
-    //Writes output file
-    //First line: v (number of vertices) and e (number of edges)
-    fprintf(output, "%u %u\n", v, e);
+  fprintf(output, "%u %u\n", v, e);
+  srandom(seed);
 
-    //Generates edges to write to file
-    uint32_t source, dest, cost;
-    uint32_t i;
-    for ( i = 0; i < e; ++i ){
-        source = rand() % v;
-        dest = rand() % v;
-        cost = rand() % max_cost;
-        fprintf(output, "%u %u %u\n", source, dest, cost);
-    }
+  uint32_t source, dest, cost;
+  for (uint32_t i = 0; i < e; ++i) {
+    source = random() % v;
+    dest = random() % v;
+    cost = random() % max;
+    fprintf(output, "%u %u %u\n", source, dest, cost);
+  }
 
-    fclose(output);
+  fclose(output);
 
-    return 0;
+  return 0;
 }
