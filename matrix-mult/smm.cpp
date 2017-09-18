0004386dad92e818e07c3f90bcdd27c79514f17c
Laercio Lima Pilla <laercio.pilla@ufsc.br>
2017-09-18 19:13:38 -0300

    mm: sparse matrix multiplication code

---
+++ matrix-mult/smm.cpp
@@ -0,0 +1,80 @@
+#include <algorithm>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <valarray>
+
+template <class T> struct matrix {
+  matrix(size_t _r, size_t _c)
+      : r(_r), c(_c), data(std::valarray<T>(0, _r * _c)) {}
+
+  T &operator()(size_t _r, size_t _c) { return data[_r * c + _c]; }
+
+  friend std::ostream &operator<<(std::ostream &os, const matrix &m) {
+    uint32_t cells = std::count_if(std::begin(m.data), std::end(m.data),
+                                   [](const T &d) { return d != 0; });
+    if (cells != 0) {
+      os << m.r << " " << m.c << " " << cells << std::endl;
+    }
+
+    for (size_t i = 0; i < m.r; ++i) {
+      for (size_t j = 0; j < m.c; ++j) {
+        T elem = m.data[i * m.c + j];
+        if (elem != 0) {
+          os << i << " " << j << " " << elem << std::endl;
+        }
+      }
+    }
+
+    return os;
+  }
+
+  const size_t r, c;
+  std::valarray<T> data;
+};
+
+matrix<int32_t> read_matrix(const char *path) {
+  try {
+    std::ifstream file(path);
+    size_t r, c;
+    int32_t value;
+
+    file >> r >> c >> value;
+    matrix<int32_t> m(r, c);
+
+    while (file >> r >> c >> value) {
+      m(r, c) = value;
+    }
+
+    return m;
+  } catch (const std::exception &e) {
+    return matrix<int32_t>(0, 0);
+  }
+}
+
+int32_t main(int32_t argc, char **argv) {
+  if (argc != 3) {
+    std::cout << "Usage: " << argv[0] << " m_file_1 m_file_2" << std::endl;
+    return 1;
+  }
+
+  matrix<int32_t> a = read_matrix(argv[1]), b = read_matrix(argv[2]);
+  if (a.c != b.r) {
+    std::cout << "Sizes are not compatible!" << std::endl;
+    return 1;
+  }
+
+  matrix<int32_t> c(a.r, b.c);
+
+  for (size_t i = 0; i < a.r; ++i) {
+    for (size_t j = 0; j < b.c; ++j) {
+      for (size_t k = 0; k < a.c; ++k) {
+        c(i, j) = c(i, j) + a(i, k) * b(k, j);
+      }
+    }
+  }
+
+  std::cout << c;
+
+  return 0;
+}
