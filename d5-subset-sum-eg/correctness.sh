b9ef5e088a8f6f0a795f91a4e489c9057beb0cdf
Laercio Lima Pilla <laercio.pilla@ufsc.br>
2017-10-20 17:05:55 -0200

    Exemplo simples adicional no correctness.sh

--- d5-subset-sum-eg/correctness.sh
+++ d5-subset-sum-eg/correctness.sh
@@ -2,5 +2,6 @@
 
 EXEC=./subsum
 
+diff <(${EXEC} 10 53 32 10) <(echo "Number of subsets = 3")
 diff <(${EXEC} 30 100 146 20) <(echo "Number of subsets = 1343")
 diff <(${EXEC} 32 1000 2000 1) <(echo "Number of subsets = 870")
