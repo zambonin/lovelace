69887d48026b1b707c380b3296abdb7b8dc3eef6
Gustavo Zambonin <gzmbnn@gmail.com>
2018-01-28 18:52:02 -0200

    sw: use int_fast##_t types * actually test the C version

--- smith-waterman/sw+.cpp
+++ smith-waterman/sw+.cpp
@@ -1,35 +1,41 @@
-#include <algorithm>
+#include <cstdint>
 #include <fstream>
 #include <iostream>
 #include <vector>
 
-#define MAX 4999
-
-int smith_waterman(const std::string &seq1, const std::string &seq2, int match,
-                   int mismatch, int gap) {
-  size_t r = seq1.size() + 1, c = seq2.size() + 1, i, j;
-  std::vector<int> matrix(2 * c);
-  int diag = 0, up = 0, left = 0, score = 0, max = 0, sim = 0;
+uint32_t smith_waterman(const std::string &seq1, const std::string &seq2,
+                        int32_t match, int32_t mismatch, int32_t gap) {
+  int32_t r = seq1.size() + 1, c = seq2.size() + 1;
+  std::vector<int32_t> matrix(2 * c);
+  int_fast32_t diag = 0, up = 0, _left = 0, score = 0, _max = 0, sim = 0, i, j;
 
   for (i = 1; i < r; i++) {
     for (j = 1; j < c; j++) {
       sim = (seq1[i - 1] == seq2[j - 1]) ? match : mismatch;
       diag = matrix[(i - 1) % 2 * c + (j - 1)] + sim;
       up = matrix[(i - 1) % 2 * c + j] + gap;
-      left = matrix[i % 2 * c + (j - 1)] + gap;
-      score = std::max({left, diag, up, 0});
-      max = std::max(max, score);
+      _left = matrix[i % 2 * c + (j - 1)] + gap;
+      score = up;
+      if (diag > up) {
+        score = diag;
+      }
+      if (_left > score) {
+        score = _left;
+      }
+      if (score > _max) {
+        _max = score;
+      }
       matrix[i % 2 * c + j] = score;
     }
   }
 
-  return max;
+  return _max;
 }
 
-int main(int argc, char **argv) {
+int32_t main(int32_t argc, char **argv) {
   if (argc < 3) {
     std::cout << "Usage: " << argv[0] << " seqfile1 seqfile2" << std::endl;
-    return 1;
+    exit(EXIT_FAILURE);
   }
 
   std::ifstream f1(argv[1]), f2(argv[2]);
@@ -37,16 +43,7 @@ int main(int argc, char **argv) {
   f1 >> seq1;
   f2 >> seq2;
 
-  if (seq1.size() > MAX) {
-    seq1 = seq1.substr(0, MAX);
-  }
-
-  if (seq2.size() > MAX) {
-    seq2 = seq2.substr(0, MAX);
-  }
-
   std::cout << "Max Score is " << smith_waterman(seq1, seq2, 2, -1, -1)
             << std::endl;
-
-  return 0;
+  exit(EXIT_SUCCESS);
 }
--- smith-waterman/sw.c
+++ smith-waterman/sw.c
@@ -3,20 +3,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-int smith_waterman(const char *seq1, const char *seq2) {
-  int len1 = strlen(seq1), len2 = strlen(seq2);
-  int *matrix = calloc(len1 * len2, sizeof(int));
+uint32_t smith_waterman(const char *seq1, const char *seq2) {
+  int32_t len1 = strlen(seq1), len2 = strlen(seq2);
+  int32_t *matrix = calloc(len1 * len2, sizeof(int32_t));
   if (matrix == NULL) {
-    return -1;
+    return 0;
   }
 
-  int diag = 0, up = 0, left = 0, score = 0, max = 0, s = 0;
-  for (int i = 1; i < len1; ++i) {
-    for (int j = 1; j < len2; ++j) {
-      s = (seq1[i - 1] == seq2[j - 1]) ? 2 : -1;
-      diag = matrix[(i - 1) * len2 + (j - 1)] + s;
-      up = matrix[(i - 1) * len2 + j] - 1;
-      left = matrix[i * len2 + j - 1] - 1;
+  int_fast32_t diag = 0, up = 0, left = 0, score = 0, max = 0, sim = 0, i, j,
+               match = 2, mismatch = -1, gap = -1;
+  for (i = 1; i < len1; ++i) {
+    int_fast8_t seq_char = seq1[i - 1];
+    for (j = 1; j < len2; ++j) {
+      sim = (seq_char == seq2[j - 1]) ? match : mismatch;
+      diag = matrix[(i - 1) * len2 + (j - 1)] + sim;
+      up = matrix[(i - 1) * len2 + j] + gap;
+      left = matrix[i * len2 + j - 1] + gap;
       score = up;
       if (diag > up) {
         score = diag;
@@ -30,20 +32,21 @@ int smith_waterman(const char *seq1, const char *seq2) {
       matrix[i * len2 + j] = score;
     }
   }
+
   free(matrix);
   return max;
 }
 
-int main(int argc, char **argv) {
+int32_t main(int32_t argc, char **argv) {
   if (argc < 3) {
     printf("Usage: %s seqfile1 seqfile2\n", argv[0]);
-    return 1;
+    exit(EXIT_FAILURE);
   }
 
   FILE *file1 = fopen(argv[1], "re"), *file2 = fopen(argv[2], "re");
   if (file1 == NULL || file2 == NULL) {
     printf("Couldn't open sequence file(s).\n");
-    return 1;
+    exit(EXIT_FAILURE);
   }
 
   size_t len1 = 0, len2 = 0;
@@ -51,15 +54,14 @@ int main(int argc, char **argv) {
 
   while ((read = getline(&seq1, &len1, file1)) != -1) {
   }
+  fclose(file1);
+
   while ((read = getline(&seq2, &len2, file2)) != -1) {
   }
-
-  printf("Max Score is %d\n", smith_waterman(seq1, seq2));
-
-  free(seq1);
-  free(seq2);
-  fclose(file1);
   fclose(file2);
 
-  return 0;
+  printf("Max Score is %u\n", smith_waterman(seq1, seq2));
+  free(seq1);
+  free(seq2);
+  exit(EXIT_SUCCESS);
 }
